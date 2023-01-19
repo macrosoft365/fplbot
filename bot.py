5a5fc4ec388e39546e857c3ba5f345a3897cd631
macrosoft365 <danielmacmiller@gmail.com>
2023-01-18 20:35:01 -0700

    Code now prints rank, name, team name, and points

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-import discord, requests, json, creds
-import pandas as pd
+import discord, requests, creds
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,7 +21,10 @@ async def on_message(message):
         id = str(message.content)
         response = requests.get(f'https://fantasy.premierleague.com/api/leagues-classic/{id}/standings/?page_standings=1')
         parsed = response.json()
-        print(parsed)   
-        await message.channel.send(f'Here are the most recent standings for your league: ')
-        
+        standings = parsed['standings']
+        results = standings['results']
+        await message.channel.send('Here are the most recent standings for your mini league: \n')
+        for index in range(len(results)):
+            team = ("#" + str(results[index]['rank']) + " " + results[index]['player_name'] + " aka " + results[index]['entry_name'] + " with " + str(results[index]['total']) + " points")
+            await message.channel.send(f'{team}')
 client.run(creds.token)
